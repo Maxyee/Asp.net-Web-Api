1f95c5d93a5adecd814d2cecf0b784faf22e4336
MD. JULHAS HOSSAIN (MOHON) <eyaminkhan00@gmail.com>
2017-09-14 05:05:36 +0600

    Updating the EF and Add a class library for mapping the Model and Database table and created the object

---
+++ WebApi_and_Sql_Server/WebApi_and_Sql_Server/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace WebApi_and_Sql_Server
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ WebApi_and_Sql_Server/WebApi_and_Sql_Server/Controllers/EmployeesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DataAccess;
+
+namespace WebApi_and_Sql_Server.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using (EmployeeApiDBEntities entities = new EmployeeApiDBEntities())
+            {
+                return entities.Employees.ToList();
+            }
+        }
+
+        public Employee Get(int id)
+        {
+            using (EmployeeApiDBEntities entities = new EmployeeApiDBEntities())
+            {
+                return entities.Employees.FirstOrDefault(e => e.ID == id);
+            }
+        }
+    }
+}
