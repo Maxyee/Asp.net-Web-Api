de3509a45e7e646e057ca3209f7e5132dedd329f
Maxyee <eyaminkhan00@gmail.com>
2018-01-31 16:46:41 +0600

    How to return xml data by using Api controller in a project just learnt it

--- Web-Api-Part-1/Global.asax.cs
+++ Web-Api-Part-1/Global.asax.cs
@@ -17,7 +17,6 @@ namespace Web_Api_1
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
---
+++ WebApiwithSqlserver/WebApiwithSqlserver/Controllers/EmployeesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeDataAccess;
+
+namespace WebApiwithSqlserver.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using (EmployeeApiDBEntities entities = new EmployeeApiDBEntities())
+            {
+                return entities.Employees.ToList();
+            }
+        }
+
+        public Employee Get(int id)
+        {
+            using (EmployeeApiDBEntities entities = new EmployeeApiDBEntities())
+            {
+                return entities.Employees.FirstOrDefault(e => e.ID == id);
+            }
+        }
+    }
+}
