6d2f56c878ae9191b140c0f87bf4dcef1ba89373
Maxyee <eyaminkhan00@gmail.com>
2018-02-04 05:06:46 +0600

    Learned the custom meadia type formatter in webApi

---
+++ WebApiwithSqlserver/App_Start/WebApiConfig.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Web.Http;
+using Newtonsoft.Json.Serialization;
+
+namespace WebApiwithSqlserver
+{
+    public class CustomJsonFormatter : JsonMediaTypeFormatter
+    {
+        public CustomJsonFormatter()
+        {
+            this.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+        }
+
+        public override void SetDefaultContentHeaders(Type type, HttpContentHeaders headers, MediaTypeHeaderValue mediaType)
+        {
+            base.SetDefaultContentHeaders(type, headers, mediaType);
+            headers.ContentType = new MediaTypeHeaderValue("application/json");
+        }
+    }
+
+
+
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            //you can execute your own created class which is inherited from the base class like this --->
+            config.Formatters.Add(new CustomJsonFormatter());
+
+
+            // this line only returns the Json format data
+            //config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            // this below line only returns the Xml format data
+            //config.Formatters.Remove(config.Formatters.JsonFormatter);
+
+            // this below line only returns the content body in -- text/html in given data.
+            //config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
+            //    config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            //    config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        }
+    }
+}
